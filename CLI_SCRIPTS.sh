b9920abe6a91b82f90cac16fd2f00193f87a8410
Caterpillar Coevolution <36854358+caterpillar-coevolution@users.noreply.github.com>
2023-05-06 13:30:15 -0700

    Update CLI_SCRIPTS.sh

--- CLI_SCRIPTS.sh
+++ CLI_SCRIPTS.sh
@@ -4,3 +4,8 @@
 
 # step 1: assembly with hifiasm
 hifiasm -o Tnot_hifiasm_assembly.asm -t 32 m64041_220407_192334.hifi_reads.fasta.gz
+
+# step 2: purging duplicate contigs
+# this step was performed twice
+module load python/3.9/3.9.10
+/home/u15/jaykgold/purge_dups/scripts/run_purge_dups.py -p bash mirid_hifiasm_config.json /home/u15/jaykgold/purge_dups/src Tnotatus_round2
