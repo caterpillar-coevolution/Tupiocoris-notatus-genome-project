4f4b6332e546bb41aedc65124e4f62d6811fd941
Caterpillar Coevolution <36854358+caterpillar-coevolution@users.noreply.github.com>
2023-05-06 13:34:02 -0700

    Update CLI_SCRIPTS.sh

--- CLI_SCRIPTS.sh
+++ CLI_SCRIPTS.sh
@@ -9,3 +9,8 @@ hifiasm -o Tnot_hifiasm_assembly.asm -t 32 m64041_220407_192334.hifi_reads.fasta
 # this step was performed twice
 module load python/3.9/3.9.10
 /home/u15/jaykgold/purge_dups/scripts/run_purge_dups.py -p bash mirid_hifiasm_config.json /home/u15/jaykgold/purge_dups/src Tnotatus_round2
+
+# step 3 repeatmasking
+singularity exec -B ~/trf409.linux64:/opt/trf:ro tetools_1_1.sif BuildDatabase -name Tnot_twice_purged_wLTRs.DB -engine rmblast mirid_hifi_assembly.purged.purged.fa
+singularity exec -B ~/trf409.linux64:/opt/trf:ro tetools_1_1.sif RepeatModeler -pa 32 -database Tnot_twice_purged_wLTRs.DB -LTRStruct
+singularity exec -B ~/trf409.linux64:/opt/trf:ro tetools_1_1.sif RepeatMasker -lib Tnot_twice_purged_wLTRs.DB-families.fa -xsmall -pa 32 -gff -e ncbi mirid_hifi_assembly.purged.purged.fa
